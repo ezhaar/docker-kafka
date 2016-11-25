833027dfdf1698995ccc29ff26346f637898e822
Izhar ul Hassan <ezhaar@gmail.com>
2016-11-25 11:25:29 +0100

    added docker-compose
    Signed-off-by: Izhar ul Hassan <ezhaar@gmail.com>

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-sed -i s/localhost:2181/$ZKHOST:2181/g $KAFKA_HOME/config/server.properties
-sed -i s/broker.id=0/broker.id=$BROKERID/g $KAFKA_HOME/config/server.properties
+if [[ -z "$BROKERID" ]]; then
+    # By default auto allocate broker ID
+    export BROKERID=-1
+if [[ -z "$KAFKA_PORT" ]]; then
+    export KAFKA_PORT=9092
+
+if [[ -z "$ZKHOST" ]]; then
+    export ZKHOST=zookeeper:2181
+
+sed -i s/localhost:2181/$ZKHOST/g $KAFKA_HOME/config/server.properties
+sed -i s/broker.id=0/broker.id=-1/g $KAFKA_HOME/config/server.properties
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
